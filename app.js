2f1fabee012dbb723f5decf1a1f10714a88b185a
ck2135 <charankatakam8@gmail.com>
2024-02-19 22:32:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var button = document.getElementById("changeBtn");
+var contentParagraph = document.getElementById("content");
+
+button.addEventListener("click", ()=> {
+  var image = document.createElement("img");
+  image.src = "./task.png";
+  contentParagraph.innerHTML = "";
+  contentParagraph.appendChild(image);
+});
+
+function links() {
+
+  const hamBurger = document.querySelector(".ham-burger");
+  const nav = document.querySelector(".navbar");
+  const items = document.querySelector(".items");
+
+  hamBurger.addEventListener("click", ()=> {
+    items.classList.toggle("nav-active");
+
+    hamBurger.classList.toggle("toggle");
+  });
+};
+links();
